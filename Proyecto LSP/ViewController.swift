8e1a4d9c573a18487fb96d17d13b9516827c6993
Bruno <bruno@Brunos-MacBook-Pro.local>
2020-11-18 10:27:58 -0800

    he añadido una variable

--- Proyecto LSP/ViewController.swift
+++ Proyecto LSP/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var hola:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
     }
-
-
+    
 }
 
