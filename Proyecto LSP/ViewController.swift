523487ae0e4566961a2eb35c3d13acebb592465e
Bruno <bruno@Brunos-MacBook-Pro.local>
2020-11-22 09:01:58 -0800

    Prueba de funcionamiento

--- Proyecto LSP/ViewController.swift
+++ Proyecto LSP/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var hola:String = ""
+    var hola:String = "Prueba 01"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
