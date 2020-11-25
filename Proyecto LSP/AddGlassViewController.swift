145b1586be0fb95b5e3b3a8bbd9839040e8c25f4
r <r@rs-MacBook-Pro.local>
2020-11-25 11:00:16 -0800

    Retocando el storyboard

---
+++ Proyecto LSP/AddGlassViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddGlassViewController.swift
+//  Proyecto LSP
+//
+//  Created by r on 25/11/2020.
+//  Copyright © 2020 Cristian Alvarez hossein. All rights reserved.
+//
+
+import UIKit
+
+class AddGlassViewController: UIViewController {
+    
+    var hola:String = "Prueba 01"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+}
