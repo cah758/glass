009cdc7909dc52240cca7b43b375f995973c90c3
Bruno <bruno@Brunos-MacBook-Pro.local>
2020-12-09 11:00:55 -0800

    hemos metido structs y la cosa del con la api

--- Proyecto LSP/InicioViewController.swift
+++ Proyecto LSP/InicioViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+
 class InicioViewController: UIViewController {
 
-    @IBOutlet weak var nombreUsuario: UITextField!
+    struct user:Codable{
+        let email:String
+        let pass:String
+    }
+    
+    @IBOutlet weak var email: UITextField!
+    
     @IBOutlet weak var passwordUsuario: UITextField!
     @IBOutlet weak var iniciarSesionBtn: UIButton!
     @IBOutlet weak var recordarPassBtn: UIButton!
@@ -22,7 +29,41 @@ class InicioViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func iniciarSesion(_ sender: Any) {
+        
+        let url = URL(string:"http://lps.tabalu.es/API/auth/login")
+        
+        let uploadDataModel = user(email:email.text as String!, pass:passwordUsuario.text as String!)
+        
+        guard let jsonData = try? JSONEncoder().encode(uploadDataModel) else{
+            print("iofeiofeiojefwj")
+            return
+        }
+        
+        var request = URLRequest(url:url!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField:"Content-Type")
+        request.setValue("application/json", forHTTPHeaderField:"Accept")
+        request.httpBody = jsonData
+        request.httpMethod = "POST"
+        let dataTask = URLSession.shared.dataTask(with:request){
+            data,_,error in
+            if let error = error{
+                print(error);return
+            }
+            do{
+                DispatchQueue.main.async {
+                    //AQUI VA EL SEGUE
+                }
+            }
+        }
+        dataTask.resume()
 
+        
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
