c34dad7d3dc4e514ae4b716678ef2dca91809f26
Bruno <bruno@Brunos-MacBook-Pro.local>
2021-01-12 11:53:51 -0800

    El slider funciona con las imagenes

--- Proyecto LSP/AddGlassViewController.swift
+++ Proyecto LSP/AddGlassViewController.swift
@@ -14,7 +14,8 @@ class AddGlassViewController: UIViewController{
     @IBOutlet weak var guardar: UIBarButtonItem!
     
 
-    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var iRImg: UIImageView!
+    
     
     @IBOutlet weak var labelAl: UILabel!
     
@@ -30,6 +31,14 @@ class AddGlassViewController: UIViewController{
     
     @IBOutlet weak var pickerView: UIPickerView!
     
+   
+    @IBOutlet weak var alImg: UIImageView!
+    
+    @IBOutlet weak var baImg: UIImageView!
+    
+    @IBOutlet weak var mgImg: UIImageView!
+    
+    @IBOutlet weak var naImg: UIImageView!
     
     var cristales = ["Cristal de construcción flotado", "Cristal de construcción no flotado", "Cristal de vehículo flotado", "Cristal de vehículo no flotado", "Cristal de contenedor", "Cristal de cubertería", "Cristal de luz de coche"]
     
@@ -41,12 +50,80 @@ class AddGlassViewController: UIViewController{
         self.title = "Nuevo cristal"
         pickerView.delegate = self
         pickerView.dataSource = self
-        sliderLabel.text = ""
+        
         sliderIndRef.minimumValue = 1.5112
         sliderIndRef.maximumValue = 1.5339
+        sliderIndRef.isUserInteractionEnabled = true
         
-        
+       
     }
+    
+    
+    @IBAction func didTapImageView(_ sender: UITapGestureRecognizer) {
+       
+        alImg.isHighlighted = true
+        baImg.isHighlighted = false
+        mgImg.isHighlighted = false
+        naImg.isHighlighted = false
+        iRImg.isHighlighted = false
+        
+        sliderIndRef.setValue(0.29, animated: true)
+        sliderIndRef.minimumValue = 0.29
+        
+        sliderIndRef.maximumValue = 3.5
+    }
+    
+    @IBAction func didTapIR(_ sender: UITapGestureRecognizer) {
+        
+      
+        alImg.isHighlighted = false
+        baImg.isHighlighted = false
+        mgImg.isHighlighted = false
+        naImg.isHighlighted = false
+        iRImg.isHighlighted = true
+        sliderIndRef.setValue(1.5112, animated: true)
+        sliderIndRef.minimumValue = 1.5112
+        sliderIndRef.maximumValue = 1.5339
+    }
+    
+    
+    @IBAction func tapImageNa(_ sender: UITapGestureRecognizer) {
+        
+        alImg.isHighlighted = false
+        baImg.isHighlighted = false
+        mgImg.isHighlighted = false
+        naImg.isHighlighted = true
+        iRImg.isHighlighted = false
+        sliderIndRef.minimumValue = 10.73
+        sliderIndRef.maximumValue = 17.38
+        sliderIndRef.setValue(10.73, animated: true)
+    }
+    
+    
+    @IBAction func tapImageBa(_ sender: UITapGestureRecognizer) {
+        alImg.isHighlighted = false
+        baImg.isHighlighted = true
+        mgImg.isHighlighted = false
+        naImg.isHighlighted = false
+        iRImg.isHighlighted = false
+        
+        sliderIndRef.minimumValue = 0
+        sliderIndRef.maximumValue = 3.15
+        sliderIndRef.setValue(0, animated: true)
+    }
+    
+    @IBAction func tapImageMg(_ sender: UITapGestureRecognizer) {
+        alImg.isHighlighted = false
+        baImg.isHighlighted = false
+        mgImg.isHighlighted = true
+        naImg.isHighlighted = false
+        iRImg.isHighlighted = false
+        sliderIndRef.minimumValue = 0
+        sliderIndRef.maximumValue = 4.49
+        sliderIndRef.setValue(0, animated: true)
+    }
+ 
+    
     
     @IBAction func guardarCristal(_ sender: Any) {
         //Hace cosas con la BBDD
@@ -61,9 +138,21 @@ class AddGlassViewController: UIViewController{
         performSegue    (withIdentifier: "cancelar", sender: self)
         
     }
-    @IBAction func sliderValueChanged(_ sender: Any) {
-        //sliderLabel.text = "\(sliderIndRef.value)"
-        sliderLabel.text = String(format:"%.4f",sliderIndRef.value)
+    
+    @IBAction func sliderValueChanged(_ sender: Any?){
+
+        if(alImg.isHighlighted){
+            labelAl.text = String(format:"%.2f",sliderIndRef.value)
+        }else if(baImg.isHighlighted){
+            labelBa.text = String(format:"%.2f",sliderIndRef.value)
+        }else if(mgImg.isHighlighted){
+            labelMg.text = String(format:"%.2f", sliderIndRef.value)
+        }else if(naImg.isHighlighted){
+            labelNa.text = String(format:"%.2f", sliderIndRef.value)
+        }else if(iRImg.isHighlighted){
+            labelIR.text = String(format:"%.4f",sliderIndRef.value)
+        }
+        
     }
     
     
@@ -90,8 +179,3 @@ extension AddGlassViewController: UIPickerViewDelegate, UIPickerViewDataSource{
     
 }
 
-extension AddGlassViewController: UIImagePickerControllerDelegate{
-    
-    
-    
-}
